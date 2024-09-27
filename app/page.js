7648bad8ad2e129d54f68a8bdb434a8bb9f71376
JPivista <jpivistasolutions@gmail.com>
2024-09-27 15:47:30 +0530

    tailwind updated

--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import './globals.css'
 import HomeComponent from "../Components/HomeComponents"
 export default function Home() {
   return (
     <>
-    <HomeComponent />
+      <HomeComponent />
     </>
   );
 }
